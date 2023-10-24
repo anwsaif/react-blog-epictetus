cd35f347dfea0d0704f4bfcbe5e3db298a9db53e
Anwar S <anwarxsaifullah+github@gmail.com>
2023-10-24 06:12:25 +0000

    [Desktop] Update Navbar: add nav items

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/'/>
+        </Routes>
+      </Router>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,31 @@
+import { Link } from "react-router-dom"
+
 export default function Navbar(){
+  const navMenu = [
+    { link: "UI Design", path: "ui-design" },
+    { link: "Front-end", path: "front-end" },
+    { link: "Back-end", path: "back-end" },
+  ]
   return (
     <header className="flex justify-between p-[30px]">
       <div>
-        <span>i</span>
-        <span>j</span>
-        <span>k</span>
+        <div id="nav-button" className="md:hidden">
+          <span>bu</span>
+          <span>r</span>
+          <span>ger</span>
+        </div>
+        <nav className="hidden md:block">
+          <ul className="gap-10 md:flex">
+            {navMenu.map(({ link, path }) => <li key={path}><Link  to={path}>{link}</Link></li> )}
+          </ul>
+        </nav>
+      </div>
+      <div className="md:order-first">
+        <span>Epic</span>
+        <span>tetus</span>
       </div>
       <div>
-        <span>p</span>
-        <span>k</span>
-      </div>
-      <div>
-j
+        search
       </div>
     </header>
   )
