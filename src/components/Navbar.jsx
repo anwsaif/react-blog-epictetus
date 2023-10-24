91d3480bf49cb69956df7b15e97cc9c2fd5f9d6a
Anwar S <anwarxsaifullah+github@gmail.com>
2023-10-24 10:39:15 +0000

    [Desktop] Update Navbar: add nav items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,8 @@ export default function Navbar(){
     { link: "UI Design", path: "ui-design" },
     { link: "Front-end", path: "front-end" },
     { link: "Back-end", path: "back-end" },
+    { link: "DevOps", path: "devops" },
+    { link: "Cyber Security", path: "cyber-sec" },
   ]
   return (
     <header className="flex items-center justify-between p-[30px]">
@@ -16,7 +18,17 @@ export default function Navbar(){
         </div>
         <nav className="hidden md:block">
           <ul className="gap-10 md:flex">
-            {navMenu.map(({ link, path }) => <li key={path}><Link  to={path}>{link}</Link></li> )}
+            {navMenu.slice(0,3).map(({ link, path }) => {
+              return (
+                <li key={path}>
+                  <Link to={path}>{link}</Link>
+                </li>
+              )
+            })}
+            <button className="inline">
+              Lainnya <span><svg className="inline" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M4 6L8 10L12 6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg> </span></button>
           </ul>
         </nav>
       </div>
