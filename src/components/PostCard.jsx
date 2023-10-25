760b12e3dcfd2018ada92791acb988af0fda9c71
Anwar S <anwarxsaifullah+github@gmail.com>
2023-10-25 20:41:59 +0000

    [Mobile, Desktop] Update PostCard: fix responsive
    add aspect ratio to the image container, add object-{fit,position}

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,16 +1,26 @@
 export default function PostCard(){
-  const img = 'post-2.jpg';
+  // const img = 'post-2.jpg';
+  const img = 'https://source.unsplash.com/400x1000?billie+eilish'
   return (
-    <div className="flex w-full flex-col gap-y-5 sm:w-1/3">
-      <div className="relative h-[213px] w-auto overflow-hidden rounded-[10px]">
-        <img src={img} className={`w-full absolute`} />
+    <div className="flex w-full flex-col gap-y-5 sm:w-2/5">
+      <div className="relative aspect-video w-full overflow-hidden rounded-[10px]">
+        <img src={img} className='h-full w-full object-cover object-center' />
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-y-3">
         <div className="flex items-center gap-3 text-[rgba(255,255,255,0.60)]">
           <span>INTERNET</span>
           <span className="h-1 w-1 rounded-full bg-[rgba(255,255,255,0.60)]"></span>
           <span>June 28, 2021</span>
         </div>
+        <p className="text-xl">
+          How to design a product that can grow itself 10x in year
+        </p>
+        <p className="opacity-60">
+          Auctor Porta. Augue vitae diam mauris faucibus blandit elit per, feugiat leo dui orci. Etiam vestibulum. Nostra netus per conubia dolor.
+        </p>
+      </div>
+      <div className="flex">
+        <img src="" alt=""/>
       </div>
     </div>
   )
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,7 +2,7 @@ import PostCard from "./PostCard"
 
 export default function Posts(){
   return (
-    <section className="px-[30px]">
+    <section className="px-[30px] lg:mt-14">
       <div className="container mx-auto">
         <div className="flex flex-wrap items-center justify-center gap-x-7 gap-y-16 lg:flex-nowrap">
           <PostCard />
