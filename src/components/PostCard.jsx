e3995c60f60db987795d572d526196ceba7994f5
Anwar S <anwarxsaifullah+github@gmail.com>
2023-10-27 16:16:27 +0000

    [Desktop] Update PostCard: add flex grow

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,16 +1,16 @@
 import { useOutletContext } from "react-router-dom"
 
-const PostCard = () => {
+const PostCard = (props) => {
   const { postImagePath, authorImagePath } = useOutletContext()
 
   return (
-    <div className="flex w-full flex-col gap-y-5 sm:w-2/5">
+    <div className="flex w-full grow flex-col gap-y-5 sm:w-2/5 lg:w-3/12">
       <div className="relative aspect-video w-full overflow-hidden rounded-[10px]">
-        <img src={img} className='h-full w-full object-cover object-center' />
+        <img src={postImagePath + props.data.image} className='h-full w-full object-cover object-center' />
       </div>
       <div className="flex flex-col gap-y-3">
         <div className="flex items-center gap-3 text-[rgba(255,255,255,0.60)]">
-          <span>INTERNET</span>
+          <span>{props.data.category}</span>
           <span className="h-1 w-1 rounded-full bg-[rgba(255,255,255,0.60)]"></span>
           <span>June 28, 2021</span>
         </div>
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,13 +1,16 @@
 import PostCard from "./PostCard";
+import data from '../../public/data.json'
 
 export default function Posts(){
   return (
-    <section className="px-[30px] lg:mt-14">
+    <section className="px-[30px] sm:mt-8 lg:mt-14">
       <div className="container mx-auto">
-        <div className="flex flex-wrap items-center justify-center gap-x-7 gap-y-16 lg:flex-nowrap">
-          {/* <PostCard /> */}
-          {/* <PostCard /> */}
-          {/* <PostCard /> */}
+        <div className="flex flex-wrap items-center justify-center gap-x-7 gap-y-16">
+          {data.map((d,i) => {
+            return (
+              <PostCard key={i} data={d}/>
+            )
+          })}
         </div>
       </div>
     </section>
